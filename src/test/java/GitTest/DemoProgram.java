a45a200dac6cdec21c36f0838a80a815b8fef818
Aditya Parkar <adityaparkar259@gmail.com>
2025-12-17 15:05:21 +0530

    commit from Asian timezone

--- src/test/java/GitTest/DemoProgram.java
+++ src/test/java/GitTest/DemoProgram.java
@@ -9,6 +9,9 @@ public class DemoProgram {
 	public void demo1() {
 		
 		System.out.println("This is Demo1");
+		System.out.println("This is Demo2");
+		System.out.println("This is Demo3");
+		
 	}
 
 }
