c63e21a82e1a4f5b9f31ac83217533b82d484aa3
Aditya Parkar <adityaparkar259@gmail.com>
2025-12-17 16:35:47 +0530

    American development team

--- src/test/java/GitTest/DemoProgram.java
+++ src/test/java/GitTest/DemoProgram.java
@@ -3,26 +3,33 @@ package GitTest;
 import org.testng.annotations.Test;
 
 public class DemoProgram {
-	
-	
+
 	@Test
 	public void demo1() {
-		
+
 		System.out.println("This is Demo1");
 		System.out.println("This is Demo2");
 		System.out.println("This is Demo3");
-		
+
 		// this is edited by American timezone
 		System.out.println("This is Demo4");
 		System.out.println("This is Demo5");
 	}
-	
-	
+
+	@Test
 	public void iosApps() {
-		
+
 		System.out.println("This is VN app");
 		System.out.println("This is Safari app");
-		
+
+	}
+
+	@Test
+	public void androidApps() {
+
+		System.out.println("This is Swiggy app");
+		System.out.println("This is Zomato app");
+
 	}
 
 }
