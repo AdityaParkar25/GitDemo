393ba4033f19a8dd0beeb8200caec0cfe09556dc
Aditya Parkar <adityaparkar259@gmail.com>
2025-12-17 16:42:21 +0530

    Asian team

---
+++ src/test/java/GitTest/DemoFile.java
@@ -0,0 +1,14 @@
+package GitTest;
+
+import org.testng.annotations.Test;
+
+public class DemoFile {
+	
+	@Test
+	public void carNames() {
+		
+		System.out.println("This is BMW");
+		System.out.println("This is Audi");
+	}
+
+}
